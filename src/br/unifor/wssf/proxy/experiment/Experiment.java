cd76bd4c847e0a9038a2b27daa44c9d4da75018d
Tulio Monte Azul <tuliomonteazul@gmail.com>
2011-10-07 19:04:00 -0300

    Adicionado TODO para medir consumo de bateria

--- src/br/unifor/wssf/proxy/experiment/Experiment.java
+++ src/br/unifor/wssf/proxy/experiment/Experiment.java
@@ -15,6 +15,8 @@ public class Experiment {
 	private Integer firstConnectionTime = new Integer(0);
 	private Integer firstReadTime = new Integer(0);
 	
+	// TODO adicionar consumo de bateria
+	
 	//private StringBuffer details;
 
 	public Integer getFirstConnectionTime() {
