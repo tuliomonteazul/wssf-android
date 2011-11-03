e8c3d783d5f1923c30099651d36023840414c7ac
Tulio Monte Azul <tuliomonteazul@gmail.com>
2011-11-03 08:11:53 -0300

    Permitindo repetir ciclos de execuções especificando no arquivo executionSetup.

--- src/br/unifor/wssf/experiment/executor/ExecutionSetup.java
+++ src/br/unifor/wssf/experiment/executor/ExecutionSetup.java
@@ -11,26 +11,47 @@ import br.unifor.wssf.core.replicas.TextFileReplicaDAO;
 
 public class ExecutionSetup {
 	private static final String SETUP_FILE = "execution-setup";
+	private final List<Execution> executions = new ArrayList<Execution>();
+	private final List<Execution> execsToRepeat = new ArrayList<Execution>();
+	private int repeat;
 	
 	public List<Execution> readSetupFile() throws IOException {
-		final List<Execution> executions = new ArrayList<Execution>();
 		final File file = new File(TextFileReplicaDAO.REPLICA_FILE_PATH + SETUP_FILE);
 		final BufferedReader reader = new BufferedReader(new FileReader(file));
 		
 		String line = reader.readLine();
+		
 		while (line != null) {
-			if (!isCommentLine(line)) {
-				executions.add(new Execution(line));
+			boolean isRepeatLine = checkRepeat(line);
+			
+			if (!isCommentLine(line) && !isRepeatLine) {
+				final Execution execution = new Execution(line);
+				executions.add(execution);
+				if (repeat > 0) {
+					execsToRepeat.add(execution);
+				}
 			}
 			
 			line = reader.readLine();
 		}
 		
+		for (int i = 1; i < repeat; i++) {
+			executions.addAll(execsToRepeat);
+		}
+		
 		return executions;
 	}
 
+	private boolean checkRepeat(String line) {
+		boolean isRepeatLine = false;
+		if (line.toLowerCase().startsWith("repeat".toLowerCase())) {
+			repeat = Integer.parseInt(line.split(" ")[1]);
+			isRepeatLine = true;
+		}
+		return isRepeatLine;
+	}
+	
 	private boolean isCommentLine(String line) {
 		return line.charAt(0) == '#';
 	}
-	
 }
