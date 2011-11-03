7047c1815f95b5ccc8215d33dbb1e3b04d60cff5
Tulio Monte Azul <tuliomonteazul@gmail.com>
2011-11-03 08:26:08 -0300

    Adicionado classe BatteryStatus para acessar o nivel de bateria.

---
+++ src/br/unifor/wssf/view/execution/battery/BatteryStatus.java
@@ -0,0 +1,30 @@
+package br.unifor.wssf.view.execution.battery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+public class BatteryStatus {
+
+	private Context context;
+
+	public BatteryStatus(final Context context) {
+		super();
+		this.context = context;
+	}
+
+	public float getLevel() {
+		
+		Intent intent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+		
+		final int intLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		final int intScale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+		return intLevel / intScale;
+	}
+	
+	public Context getContext() {
+		return context;
+	}
+}
