047fce2bf653faeed0cee2da5abee7dcc45a4b9c
Tulio Monte Azul <tuliomonteazul@gmail.com>
2011-11-03 08:22:08 -0300

    Utilizando classe BatteryStatus na execucao multipla.

--- src/br/unifor/wssf/view/execution/multiple/MultiExecActivity.java
+++ src/br/unifor/wssf/view/execution/multiple/MultiExecActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ScrollView;
 import br.unifor.wssf.R;
 import br.unifor.wssf.experiment.executor.MultipleExecutor;
-import br.unifor.wssf.view.widget.LogEditText;
+import br.unifor.wssf.view.execution.battery.BatteryStatus;
+import br.unifor.wssf.view.widget.LogTextView;
 
 public class MultiExecActivity extends Activity {
 	
@@ -16,23 +18,26 @@ public class MultiExecActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.multi_exec);
 		
-		
-		
 		addListenerBtIniciarExperimentos();
+		
 	}
 	
 	private void addListenerBtIniciarExperimentos() {
-		final MultiExecActivity multiExecActivity = this;
-		final LogEditText logExecucao = (LogEditText) findViewById(R.id.etLogExecucao);
+		final LogTextView logExecucao = (LogTextView) findViewById(R.id.etLogExecucao);
+		final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollLog);
+		logExecucao.setScrollView(scrollView);
+		
 		Button btIniciarExperimentos = (Button) findViewById(R.id.btExperimento);
+		
+		final BatteryStatus batteryStatus = new BatteryStatus(this);
+		
 		btIniciarExperimentos.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				MultipleExecutor multipleExecutor = new MultipleExecutor(multiExecActivity, logExecucao);
+				MultipleExecutor multipleExecutor = new MultipleExecutor(batteryStatus, logExecucao);
 				multipleExecutor.start();
 			}
 		});
 	}
-
 }
